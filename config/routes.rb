d0e10d3a977d3e89a6a3ac8573ca9db8420cf6f0
Tara Sjarif <tarasjarif@gmail.com>
2018-04-16 21:37:43 -0700

    create all views and controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "attractions#index"
-  resources :attractions
+  root "categories#index"
+  resources :categories
 
-  resources :users, only: [:new, :create]
+  get "/category/:id", to: "categories#show"
+
+  resources :users, only: [:new, :create, :show]
 
   resources :sessions, only: [:new, :create, :destroy]
   get "/login", to: "sessions#new"
   get "/logout", to: "sessions#destroy"
+
+  resources :attractions, only: [:show, :index]
+
+  
 
 end
  
