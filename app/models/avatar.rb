77b5942022791bd1017a790a1b715a72c8ae9fc7
Tara Sjarif <tarasjarif@gmail.com>
2018-04-19 10:25:49 -0700

    fix index category link

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,3 +1,3 @@
 class Avatar < ApplicationRecord
-    
+    has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_secure_password
     validates :name, presence: true, uniqueness: true
     has_many :reviews, dependent: :destroy
+    belongs_to :avatar
 end
